3babbe6bd64a04b0c32dd1168dfee755f7670a2b
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2023-11-25 18:11:06 -0300

    refactor: isolate argument use-browser handling + new fixture for already logged scenarios

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from tests.pages.InventoryPage import InventoryPage
 
 from tests.pages.LoginPage import LoginPage
 
@@ -19,13 +20,33 @@ def pytest_addoption(parser):
         help="Define the browser to be executed. Current supported versions: " + ", ".join(SUPPORTED_BROWSERS_LIST)
     )
 
+@pytest.fixture(scope="function")
+def use_browser_option(request):
+    """Fixture to handle the use of argument --use-browser to select"""
+    selected_browser = request.config.getoption("--use-browser")
+    return selected_browser
+
 
 @pytest.fixture(scope="function")
-def start_by_login_page(request):
+def start_by_login_page(use_browser_option):
     """Fixture that starts the WebDriver by accessing the Login page"""
-    selected_browser = request.config.getoption("--use-browser")
-    login_page = LoginPage(driver=selected_browser)
+    
+    login_page = LoginPage(driver=use_browser_option)
     yield login_page
     
     login_page.close_driver()
 
+
+@pytest.fixture(scope="function")
+def start_already_logged(use_browser_option):
+    """Fixture that starts the WebDriver with an user already logged"""
+    
+    login_page = LoginPage(driver=use_browser_option)
+    login_page.access_page()
+    login_page.type_successfull_credentials()
+    login_page.click_on_login()
+    inventory_page = InventoryPage(driver = login_page.driver)
+
+    yield inventory_page
+    
+    inventory_page.close_driver()
