ca71a0e6765fd70bfd8d7ab7583bb792a30f133b
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2023-11-29 22:15:41 -0300

    chore: fixture to handle pytest.mark to use specific browser

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from tests.pages.InventoryPage import InventoryPage
 
 from tests.pages.LoginPage import LoginPage
+from tests.pages.MenuPage import MenuPage
 
 SUPPORTED_BROWSERS_LIST = [
     "chrome",
@@ -23,8 +24,13 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def use_browser_option(request):
     """Fixture to handle the use of argument --use-browser to select"""
-    selected_browser = request.config.getoption("--use-browser")
-    return selected_browser
+    
+    force_browser = request.node.get_closest_marker("FORCE_BROWSER")
+    if force_browser is None:    
+        selected_browser = request.config.getoption("--use-browser")
+        return selected_browser
+    
+    return force_browser.args[0]
 
 
 @pytest.fixture(scope="function")
@@ -50,3 +56,16 @@ def start_already_logged(use_browser_option):
     yield inventory_page
     
     inventory_page.close_driver()
+
+
+@pytest.fixture(scope="function")
+def start_with_one_product_added(start_already_logged):
+    """Fixture that starts the WebDriver with an user logged and at least one product added"""
+    inventory_page = InventoryPage(driver = start_already_logged.driver)
+    selected_item, _, _ = inventory_page.choose_an_item_randomsly()
+    inventory_page.add_item_to_cart(selected_item)
+    
+    menu_page = MenuPage(driver = inventory_page.driver)
+    menu_page.click_on_cart_icon()
+    return menu_page
+    
